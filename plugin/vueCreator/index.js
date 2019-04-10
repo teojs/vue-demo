2f7f60a9f18f9daf6c9ddbf659ed67bf0959c199
Teo <cheesen.xu@gmail.com>
2019-04-10 15:08:16 +0800

    init

--- plugin/vueCreator/index.js
+++ plugin/vueCreator/index.js
@@ -4,27 +4,32 @@ module.exports = () => {
   const views = path.join(process.cwd(), 'src/views')
 
   fs.watch(views, { recursive: true }, (eventType, filename) => {
-    if (eventType === 'rename') {
-      if (fs.existsSync(path.join(views, filename))) {
-        // 自动写入模板
-        const file = fs.readFileSync(path.join(views, filename), {
+    if (
+      eventType === 'rename' &&
+      fs.existsSync(path.join(views, filename)) &&
+      fs.statSync(path.join(views, filename)).isFile()
+    ) {
+      // 自动写入模板
+      const file = fs.readFileSync(path.join(views, filename), {
+        encoding: 'utf-8',
+      })
+      const template = fs.readFileSync(
+        path.join(process.cwd(), 'plugin/vueCreator/tpl.vue'),
+        {
           encoding: 'utf-8',
-        })
-        const template = fs.readFileSync(
-          path.join(process.cwd(), 'plugin/vueCreator/tpl.vue'),
-          {
-            encoding: 'utf-8',
-          }
-        )
-        if (file === '') {
-          fs.writeFileSync(
-            path.join(views, filename),
-            template.replace(
-              'component name here',
-              filename.replace('.vue', '')
-            )
-          )
         }
+      )
+      if (file === '') {
+        fs.writeFileSync(
+          path.join(views, filename),
+          template.replace(
+            'component name here',
+            filename
+              .replace('.vue', '')
+              .replace('\\', '-')
+              .replace('/', '-')
+          )
+        )
       }
     }
   })
